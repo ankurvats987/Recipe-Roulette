598820e99379915bf913631fce51bd46566aea33
ankur <rockingjohnny2004@gmail.com>
2025-01-01 20:16:14 +0530

    first init

---
+++ main.js
@@ -0,0 +1,187 @@
+const recipeName = document.getElementById("recipe-name");
+const recipeMetaData = document.getElementsByClassName("recipe-metadata");
+const recipeSteps =  document.getElementById("recipe-steps");
+const recipeThumb = document.getElementById("recipe-thumb");
+const recipeIngredientsWrapper = document.getElementById("ingredient-wrapper");
+const dummyIngredients = document.getElementById("dummy-ingredients");
+
+const header = document.getElementsByTagName("header");
+
+const savedRecipeBtn = document.getElementById("show-saved-btn");
+const saveRecipeBtn = document.getElementById("save-recipe-btn");
+const saveContinueBtn = document.getElementById("save-continue-btn");
+const alreadyContinueBtn = document.getElementById("already-continue-btn");
+const cancelBtn = document.getElementById("cancel-btn");
+const deleteBtn = document.getElementById("delete-btn");
+
+const savedRecipeDialog = document.getElementById("saved-dialog");
+const deleteRecipeDialog = document.getElementById("delete-dialog");
+const alreadyThereDialog = document.getElementById("already-there-dialog");
+
+let foodData = [];
+let recipeId;
+
+function generateRandomNumber () {
+    const randomValue1 = Math.floor(Math.random() * (2 - 1 + 1)) + 1; // Random integer between 1 and 2
+    const randomValue2 = Math.floor(Math.random() * (4 - 3 + 1)) + 3; // Random integer between 3 and 4
+
+    const formattedValue = `${randomValue1} - ${randomValue2}`;
+
+   return formattedValue;
+}
+
+savedRecipeBtn.onclick = function () {
+    window.location.href = "saved_recipes.html";
+}
+
+saveRecipeBtn.addEventListener('click', () => {
+    if (foodData) {
+        if (recipeId) {
+            deleteRecipeDialog.showModal();
+            deleteRecipeDialog.style.visibility = "visible";
+        } else {
+            let savedRecipes = JSON.parse(
+                localStorage.getItem("saved-recipes"));
+
+            for (let recipe of savedRecipes) {
+                if (recipe.idMeal === foodData.idMeal) {
+                    alreadyThereDialog.showModal();
+                    alreadyThereDialog.style.visibility = "visible";
+                    console.log("Already saved");
+
+                    return;
+                }
+            }
+
+            let currentTimestamp = Date.now();
+            foodData.timeStamp = currentTimestamp;
+
+            if (!Array.isArray(savedRecipes)) {
+                savedRecipes = [];
+            }
+
+            savedRecipes.push(foodData);
+            localStorage.setItem("saved-recipes", JSON.stringify(savedRecipes));
+
+            savedRecipeDialog.showModal();
+            savedRecipeDialog.style.visibility = "visible"; 
+        }
+    } else {
+        alert("The data hasn't loaded yet.")
+    }    
+});
+
+saveContinueBtn.addEventListener('click', () => {
+    savedRecipeDialog.close();
+    savedRecipeDialog.style.visibility = "hidden";
+});
+
+alreadyContinueBtn.addEventListener('click', () => {
+    alreadyThereDialog.close();
+    alreadyThereDialog.style.visibility = "hidden";
+})
+
+cancelBtn.addEventListener('click', () => {
+    deleteRecipeDialog.close();
+    deleteRecipeDialog.style.visibility = "hidden";
+});
+
+deleteBtn.addEventListener('click', () => {
+    let savedRecipes = JSON.parse(localStorage.getItem("saved-recipes"));
+
+    savedRecipes = savedRecipes.filter((recipe) => recipe.idMeal != recipeId);
+    console.log(savedRecipes);
+    localStorage.setItem("saved-recipes", JSON.stringify(savedRecipes));
+
+    deleteRecipeDialog.close();
+    deleteRecipeDialog.style.visibility = "hidden";
+
+    window.location.href = "saved_recipes.html";
+});
+
+async function downloadImage(imageUrl) {
+    const response = await fetch(imageUrl);
+    const imageData = await response.blob();
+
+    return imageData;
+}
+
+async function getData(URL) {
+    const response = await fetch(URL);
+    const data = await response.json();
+    const foodData = data.meals[0];
+
+    return foodData;
+}
+
+async function displayData() {
+    const queryParams = new URLSearchParams(window.location.search);
+    recipeId = queryParams.get("recipe");
+
+    if (recipeId) {
+        updateChanges();
+        foodData = await getData(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${recipeId}`);
+    } else {
+        foodData = await getData("https://www.themealdb.com/api/json/v1/1/random.php");
+    }
+
+    saveRecipeBtn.disabled = false;
+
+    const recipeImageBlob = await downloadImage(foodData.strMealThumb);
+
+    recipeThumb.classList.toggle("skeleton");
+    recipeThumb.src = URL.createObjectURL(recipeImageBlob);
+    recipeName.classList.toggle("skeleton");
+    recipeName.textContent = foodData.strMeal;
+    
+    recipeMetaData[0].querySelector("span").classList.toggle("skeleton");
+    recipeMetaData[0].querySelector("span").textContent = 
+    foodData.strCategory;
+
+    recipeMetaData[1].querySelector("span").classList.toggle("skeleton");
+    recipeMetaData[1].querySelector("span").textContent = 
+    foodData.strArea;
+
+    recipeMetaData[2].querySelector("span").classList.toggle("skeleton");
+    recipeMetaData[2].querySelector("span").textContent = generateRandomNumber();
+
+    recipeSteps.textContent = foodData.strInstructions;
+
+    dummyIngredients.remove();
+    for (let index = 0; index < 20; index++) {
+        const key = `strIngredient${index + 1}`;
+        const ingredientName = foodData[key];
+        const ingredientBlob = await downloadImage(`https://www.themealdb.com/images/ingredients/${ingredientName}.png`);
+
+
+        if (ingredientName) {
+            const ingredientWrapper = document.createElement("article");
+            ingredientWrapper.innerHTML = `<div class="img-wrapper">
+                                    <img src="${URL.createObjectURL(ingredientBlob)}">
+                                </div>
+                                <span>${ingredientName}</span>
+                                `;
+            ingredientWrapper.classList.add("ingredient");
+        
+            recipeIngredientsWrapper.appendChild(ingredientWrapper);
+        }
+
+        
+    }
+
+    
+}
+
+function updateChanges() {
+    const buttonContainer = document.getElementById("button-container");
+    const homeBtn = document.createElement("button");
+    homeBtn.id = "show-saved-btn";
+    homeBtn.textContent =  "Home";
+    homeBtn.onclick = () => window.location.href = "index.html";
+
+    buttonContainer.appendChild(homeBtn);
+    saveRecipeBtn.textContent = "Delete Recipe";
+}
+
+
+displayData();
---
+++ save_list.js
@@ -0,0 +1,102 @@
+const homeBtn = document.getElementById("home-btn");
+
+const recipeList = document.getElementById("recipe-list");
+const recipeBox = document.getElementById("recipe-box");
+
+let savedRecipes = [];
+
+homeBtn.onclick = function () {
+    window.location.href = "index.html";
+}
+
+recipeList.addEventListener('click', (event) => {
+    if (event.target && event.target.matches(".delete-btn")) {
+        const card = event.target.closest(".recipe-box");
+
+        if (card) {
+            const index = Array.from(recipeList.children).indexOf(card);
+            card.classList.add("slide-out");
+
+            setTimeout(() => {
+                card.remove();
+                deleteRecipe(index);
+            }, 300);
+        }
+    } else if (event.target && event.target.closest(".recipe-box")) {
+        const card = event.target.closest(".recipe-box");
+        const clickedRecipe = savedRecipes[Array.from(recipeList.children).indexOf(card)];
+
+        window.location.href = `index.html?recipe=${clickedRecipe.idMeal}`;
+
+    }
+
+});
+
+function deleteRecipe(index) {
+    savedRecipes = savedRecipes.filter((_, i) => i !== index);
+    localStorage.setItem('saved-recipes', JSON.stringify(savedRecipes));
+}
+
+function formatTimeStamp(timestamp) {
+    let currentDate = new Date();
+    let savedDate = new Date(timestamp);
+
+    if (currentDate.toDateString() === savedDate.toDateString()) {
+        return "Today";
+    } 
+    
+    currentDate.setDate(currentDate.getDate() - 1);
+    if (currentDate.toDateString() === savedDate.toDateString()) {
+        return "Yesterday";
+    }
+
+    return savedDate.toLocaleDateString('en-IN', {
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric'
+    });
+}
+
+function deleteDummyBoxes() {
+    const dummyBoxes = recipeList.querySelectorAll(".skeleton");
+    dummyBoxes.forEach((box) => box.remove());
+}
+
+async function downloadImage(imageUrl) {
+    const response = await fetch(imageUrl);
+    const imageData = await response.blob();
+
+    return imageData;
+}
+
+async function displaySavedRecipes() {
+    let savedRecipesObj = JSON.parse(localStorage.getItem("saved-recipes"));
+
+    if (Array.isArray(savedRecipesObj)) {
+        deleteDummyBoxes();
+        savedRecipes = savedRecipesObj;
+        for (let recipe of savedRecipes) {
+            const imageBlob = await downloadImage(recipe.strMealThumb);
+
+            let recipeBox = document.createElement("article");
+
+            recipeBox.innerHTML = `
+                <img src="${URL.createObjectURL(imageBlob)}" class="box-thumb">
+                <div class="box-metadata">
+                    <div class="metadata-header">
+                        <span class="box-date">
+                        ${formatTimeStamp(recipe.timeStamp)}</span>
+                        <i title="Delete Recipe" class="delete-btn material-icons" style="font-size:16px;color:red">delete</i>
+                    </div>
+                    <span class="box-name">${recipe.strMeal}</span>
+                </div>
+                `;
+
+            recipeBox.classList.add("recipe-box");
+
+            recipeList.appendChild(recipeBox);
+        };
+    } 
+}
+
+displaySavedRecipes();
